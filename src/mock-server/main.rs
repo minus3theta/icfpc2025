e934363e079e0b823a27dfa206c5e7344f7eab49
Ryosuke839 <ryosukem16@gmail.com>
2025-09-06 00:20:11 +0900

    Implement

--- src/mock-server/main.rs
+++ src/mock-server/main.rs
@@ -1,32 +1,47 @@
 use actix_web::{App, HttpServer, Responder, post, web};
+use env_logger;
+use std::env;
 
+mod problem;
 mod server;
+
+use server::Server;
 use server::{ExploreRequest, GuessRequest, SelectRequest};
-use server::{explore, guess, select};
 
 async fn hello() -> impl Responder {
     "Hello, world!"
 }
 
 #[post("/select")]
-async fn select_server(req: web::Json<SelectRequest>) -> impl Responder {
-    web::Json(select(req.into_inner()).await)
+async fn select_server(req: web::Json<SelectRequest>, server: web::Data<Server>) -> impl Responder {
+    web::Json(server.select(req.into_inner()))
 }
 
 #[post("/explore")]
-async fn explore_server(req: web::Json<ExploreRequest>) -> impl Responder {
-    web::Json(explore(req.into_inner()).await)
+async fn explore_server(
+    req: web::Json<ExploreRequest>,
+    server: web::Data<Server>,
+) -> impl Responder {
+    web::Json(server.explore(req.into_inner()))
 }
 
 #[post("/guess")]
-async fn guess_server(req: web::Json<GuessRequest>) -> impl Responder {
-    web::Json(guess(req.into_inner()).await)
+async fn guess_server(req: web::Json<GuessRequest>, server: web::Data<Server>) -> impl Responder {
+    web::Json(server.guess(req.into_inner()))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    unsafe {
+        env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    let server = web::Data::new(Server::new());
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(server.clone())
             .route("/", web::get().to(hello))
             .service(select_server)
             .service(explore_server)
--- src/mock-server/server.rs
+++ src/mock-server/server.rs
@@ -1,4 +1,17 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::sync::RwLock;
+
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+
+use log::info;
+
+use crate::problem::Problem;
+
+pub struct Server {
+    problems: RwLock<HashMap<String, Problem>>,
+}
 
 #[derive(Deserialize)]
 pub struct SelectRequest {
@@ -13,12 +26,6 @@ pub struct SelectResponse {
     problem_name: String,
 }
 
-pub async fn select(req: SelectRequest) -> SelectResponse {
-    SelectResponse {
-        problem_name: req.problem_name.clone(),
-    }
-}
-
 #[derive(Deserialize)]
 pub struct ExploreRequest {
     id: String,
@@ -27,22 +34,15 @@ pub struct ExploreRequest {
 
 #[derive(Serialize)]
 pub struct ExploreResponse {
-    results: Vec<i8>,
+    results: Vec<Vec<i8>>,
     #[serde(rename = "queryCount")]
     query_count: usize,
-}
-
-pub async fn explore(req: ExploreRequest) -> ExploreResponse {
-    ExploreResponse {
-        results: vec![1; req.plans.len()],
-        query_count: req.plans.len(),
-    }
 }
 
 #[derive(Deserialize)]
 pub struct GuessRequest {
     id: String,
-    map: String,
+    map: GuessReuqestMap,
 }
 
 #[derive(Deserialize)]
@@ -61,7 +61,7 @@ struct GuessRequestConnection {
 
 #[derive(Deserialize)]
 struct GuessRequestRoom {
-    id: usize,
+    room: usize,
     door: usize,
 }
 
@@ -70,6 +70,98 @@ pub struct GuessResponse {
     correct: bool,
 }
 
-pub async fn guess(_req: GuessRequest) -> GuessResponse {
-    GuessResponse { correct: true }
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            problems: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub fn select(&self, req: SelectRequest) -> Result<SelectResponse, String> {
+        let problem = Problem::new(3);
+        info!("Problem generated:\n{}", problem.pretty_print());
+
+        self.problems
+            .write()
+            .unwrap()
+            .insert(req.id.clone(), problem);
+
+        Ok(SelectResponse {
+            problem_name: req.problem_name.clone(),
+        })
+    }
+
+    pub fn explore(&self, req: ExploreRequest) -> Result<ExploreResponse, String> {
+        let problems = self.problems.read().unwrap();
+        let problem = problems.get(&req.id).ok_or("Problem not found")?;
+
+        let mut results = Vec::<Vec<i8>>::new();
+        for plan in req.plans {
+            results.push(
+                problem
+                    .explore(&plan)?
+                    .into_iter()
+                    .map(|v| (v % 4) as i8)
+                    .collect(),
+            );
+        }
+        let query_count = results.len();
+        Ok(ExploreResponse {
+            results,
+            query_count,
+        })
+    }
+
+    pub fn guess(&self, req: GuessRequest) -> Result<GuessResponse, String> {
+        let problems = self.problems.read().unwrap();
+        let problem = problems.get(&req.id).ok_or("Problem not found")?;
+
+        let mut positions = Vec::<HashSet<usize>>::new();
+        for _ in 0..4 {
+            positions.push(HashSet::new());
+        }
+        for (i, room) in req.map.rooms.iter().enumerate() {
+            positions[*room as usize].insert(i);
+        }
+
+        // 2 bit の room から実際の room の割り当てを列挙する
+        for perm in positions
+            .iter()
+            .map(|p| p.iter().permutations(p.len()))
+            .multi_cartesian_product()
+        {
+            // 2 bit の room から実際の room の割り当てを作成する
+            let mut mapping = vec![usize::MAX; req.map.rooms.len()];
+            for (i, p) in perm.iter().enumerate() {
+                for (j, q) in p.iter().enumerate() {
+                    mapping[**q] = i + j * 4;
+                }
+            }
+
+            let starting_room = mapping[req.map.starting_room];
+
+            let mut connections = vec![vec![usize::MAX; 6]; req.map.rooms.len()];
+            for connection in req.map.connections.iter() {
+                connections[mapping[connection.from.room]][connection.from.door] =
+                    mapping[connection.to.room];
+                connections[mapping[connection.to.room]][connection.to.door] =
+                    mapping[connection.from.room];
+            }
+
+            let mut print_connections = format!("Starting room: {}", starting_room);
+            for i in 0..connections.len() {
+                print_connections += &format!("\n{}:", i);
+                for j in 0..connections[i].len() {
+                    print_connections += &format!(" {}", connections[i][j]);
+                }
+            }
+            info!("Attempting to guess:\n{}", print_connections);
+
+            if problem.guess(starting_room, connections)? {
+                return Ok(GuessResponse { correct: true });
+            }
+        }
+
+        Ok(GuessResponse { correct: false })
+    }
 }
