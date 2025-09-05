d3fc9a072bb0f64ed9d601c965d2b68039eefd5c
Ryosuke839 <ryosukem16@gmail.com>
2025-09-05 22:17:56 +0900

    format

--- src/mock-server/main.rs
+++ src/mock-server/main.rs
@@ -1,36 +1,37 @@
-use actix_web::{web, App, HttpServer, Responder, post};
+use actix_web::{App, HttpServer, Responder, post, web};
 
 mod server;
-use server::{SelectRequest, ExploreRequest, GuessRequest};
-use server::{select, explore, guess};
+use server::{ExploreRequest, GuessRequest, SelectRequest};
+use server::{explore, guess, select};
 
 async fn hello() -> impl Responder {
-  "Hello, world!"
+    "Hello, world!"
 }
 
 #[post("/select")]
 async fn select_server(req: web::Json<SelectRequest>) -> impl Responder {
-  web::Json(select(req.into_inner()).await)
+    web::Json(select(req.into_inner()).await)
 }
 
 #[post("/explore")]
 async fn explore_server(req: web::Json<ExploreRequest>) -> impl Responder {
-  web::Json(explore(req.into_inner()).await)
+    web::Json(explore(req.into_inner()).await)
 }
 
 #[post("/guess")]
 async fn guess_server(req: web::Json<GuessRequest>) -> impl Responder {
-  web::Json(guess(req.into_inner()).await)
+    web::Json(guess(req.into_inner()).await)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new()
-      .route("/", web::get().to(hello))
-      .service(select_server)
-      .service(explore_server)
-      .service(guess_server)
-    )
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+            .service(select_server)
+            .service(explore_server)
+            .service(guess_server)
+    })
     .bind("127.0.0.1:8080")?
     .run()
     .await
--- src/mock-server/server.rs
+++ src/mock-server/server.rs
@@ -2,76 +2,74 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct SelectRequest {
-  id: String,
-  #[serde(rename = "problemName")]
-  problem_name: String,
+    id: String,
+    #[serde(rename = "problemName")]
+    problem_name: String,
 }
 
 #[derive(Serialize)]
 pub struct SelectResponse {
-  #[serde(rename = "problemName")]
-  problem_name: String,
+    #[serde(rename = "problemName")]
+    problem_name: String,
 }
 
 pub async fn select(req: SelectRequest) -> SelectResponse {
-  SelectResponse {
-    problem_name: req.problem_name.clone(),
-  }
+    SelectResponse {
+        problem_name: req.problem_name.clone(),
+    }
 }
 
 #[derive(Deserialize)]
 pub struct ExploreRequest {
-  id: String,
-  plans: Vec<String>,
+    id: String,
+    plans: Vec<String>,
 }
 
 #[derive(Serialize)]
 pub struct ExploreResponse {
-  results: Vec<i8>,
-  #[serde(rename = "queryCount")]
-  query_count: usize,
+    results: Vec<i8>,
+    #[serde(rename = "queryCount")]
+    query_count: usize,
 }
 
 pub async fn explore(req: ExploreRequest) -> ExploreResponse {
-  ExploreResponse {
-    results: vec![1; req.plans.len()],
-    query_count: req.plans.len(),
-  }
+    ExploreResponse {
+        results: vec![1; req.plans.len()],
+        query_count: req.plans.len(),
+    }
 }
 
 #[derive(Deserialize)]
 pub struct GuessRequest {
-  id: String,
-  map: String,
+    id: String,
+    map: String,
 }
 
 #[derive(Deserialize)]
 struct GuessReuqestMap {
-  rooms: Vec<i8>,
-  #[serde(rename = "startingRoom")]
-  starting_room: usize,
-  connections: Vec<GuessRequestConnection>,
+    rooms: Vec<i8>,
+    #[serde(rename = "startingRoom")]
+    starting_room: usize,
+    connections: Vec<GuessRequestConnection>,
 }
 
 #[derive(Deserialize)]
 struct GuessRequestConnection {
-  from: GuessRequestRoom,
-  to: GuessRequestRoom,
+    from: GuessRequestRoom,
+    to: GuessRequestRoom,
 }
 
 #[derive(Deserialize)]
 struct GuessRequestRoom {
-  id: usize,
-  door: usize,
+    id: usize,
+    door: usize,
 }
 
 #[derive(Serialize)]
 pub struct GuessResponse {
-  correct: bool,
+    correct: bool,
 }
 
 pub async fn guess(_req: GuessRequest) -> GuessResponse {
-  GuessResponse {
-    correct: true,
-  }
+    GuessResponse { correct: true }
 }
