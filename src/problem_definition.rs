b5b76a9e6d04a7f885adecf99c947bddb717af8c
Ryosuke839 <ryosukem16@gmail.com>
2025-09-06 22:21:14 +0900

    Add version 2 problems

--- src/problem_definition.rs
+++ src/problem_definition.rs
@@ -3,10 +3,20 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Deserialize)]
+struct ProblemDefinitionJson {
+    name: String,
+    size: usize,
+    version: usize,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ProblemDefinition {
     pub name: String,
     pub size: usize,
+    pub version: usize,
+    pub max_plan_length: usize,
+    pub label_rewritable: bool,
 }
 
 pub struct ProblemDefinitions {
@@ -17,11 +27,29 @@ impl ProblemDefinitions {
     pub fn new() -> Self {
         let file = File::open("problems.json").unwrap();
         let reader = BufReader::new(file);
-        let definitions = serde_json::from_reader::<_, Vec<ProblemDefinition>>(reader).unwrap();
+        let definitions = serde_json::from_reader::<_, Vec<ProblemDefinitionJson>>(reader).unwrap();
         Self {
             definitions: definitions
                 .into_iter()
-                .map(|d| (d.name.clone(), d))
+                .map(|d| {
+                    (
+                        d.name.clone(),
+                        ProblemDefinition {
+                            name: d.name,
+                            size: d.size,
+                            version: d.version,
+                            max_plan_length: match d.version {
+                                1 => 18 * d.size,
+                                2 => 6 * d.size,
+                                _ => 0,
+                            },
+                            label_rewritable: match d.version {
+                                2 => true,
+                                _ => false,
+                            },
+                        },
+                    )
+                })
                 .collect(),
         }
     }
